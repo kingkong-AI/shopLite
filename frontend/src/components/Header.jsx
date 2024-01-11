2fbf0eb0b8137f666d1bfdad7a607d909451d87a
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-12 02:02:12 +0530

    Added Cards, harcoded their values for now later on to be fetched from database

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container, NavLink } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg = "dark" variant = "dark" expand = "md" collapsOnSelect>
             <Container>
-                <Navbar.Brand href = "/">ShopLite</Navbar.Brand>
+                <Navbar.Brand href = "/">
+                    <img src={logo} alt="ShopLite" />
+                    ShopLite
+                    </Navbar.Brand>
                 <Navbar.Toggle aria-controls = "basic-navbar-nav"/>
                 <Navbar.Collapse id = "basic-navbar-nav">
                 <Nav className="ms-auto">
