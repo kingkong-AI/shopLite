0f60baf8bb646999ef846b8f7b0c05bcc0fd6cba
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-13 02:26:06 +0530

    Added Ratings

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container, NavLink } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import {LinkContainer} from 'react-router-bootstrap'
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -7,16 +8,21 @@ const Header = () => {
     <header>
         <Navbar bg = "dark" variant = "dark" expand = "md" collapsOnSelect>
             <Container>
-                <Navbar.Brand href = "/">
+                <LinkContainer to='/'>
+                <Navbar.Brand>
                     <img src={logo} alt="ShopLite" />
                     ShopLite
                     </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls = "basic-navbar-nav"/>
                 <Navbar.Collapse id = "basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <NavLink href="/cart"><FaShoppingCart />Cart</NavLink>
-                    <NavLink href="/login"><FaUser />Sign In</NavLink>
-
+                    <LinkContainer to="/cart">
+                    <NavLink><FaShoppingCart />Cart</NavLink>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                    <NavLink><FaUser />Sign In</NavLink>
+                    </LinkContainer>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
