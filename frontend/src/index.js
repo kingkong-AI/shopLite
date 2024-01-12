384d7606db7994aa33131d1682c8940404616621
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-12 22:46:26 +0530

    Understading React Router and adding routes in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
 import App from './App';
