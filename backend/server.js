97c97662916d779c5a75f50c1c32e7c533055bf8
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-13 16:15:20 +0530

    Got the basic server running

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
 import express from 'express';
+const port = 8000;
+
+const app = express();
+app.get('/', (req, res)=> {
+    res.send("API is running ...");
+});
+
+app.listen(port, ()=>console.log(`Server running on port ${port}`))
