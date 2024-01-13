31b7138ac10161e8acc12699c42ef80ff4b3ea9c
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-13 16:41:02 +0530

    setting up environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import products from './data/products.js';
-const port = 8000;
+const port = process.env.PORT || 8080;
 
 const app = express();
 app.get('/', (req, res)=> {
