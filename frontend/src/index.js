61d72fae23f54b565f0d5724cc0c819c88703a97
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-13 15:00:25 +0530

    FrontEnd Basics done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
 import App from './App';
@@ -11,6 +12,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index='true' path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
@@ -25,4 +27,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//Routes are created in index.js
 reportWebVitals();
