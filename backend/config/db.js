effd0d0ba5d5e048afd26c278c768f696eb93923
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-14 14:39:49 +0530

    connected to DB, now adding schema in models

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import { Console } from "console";
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 8080;
+
+connectDB(); // connects to DB right before we initialize the app
 
 const app = express();
 app.get('/', (req, res)=> {
