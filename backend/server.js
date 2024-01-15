c6409e3c35298da0c60d50a1dad958ced282f951
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-15 14:15:28 +0530

    Created middlewares for handling exceptions etc, also added routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 8080;
 
 connectDB(); // connects to DB right before we initialize the app
@@ -12,13 +13,8 @@ app.get('/', (req, res)=> {
     res.send("API is running ...");
 });
 
-app.get('/api/products', (req, res) => {
-    res.send(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.send(product);
-});
+app.use('/api/products',productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, ()=>console.log(`Server running on port ${port}`))
