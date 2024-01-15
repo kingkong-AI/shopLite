0502a18d43be1db93926143a41b0a8a28c76b372
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-15 14:25:17 +0530

    cleaned up the routes by adding controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,9 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js"
-router.get('/', asyncHandler( async (req, res) => {
-    const products = await Product.find({});
-    res.send(products);
-}));
+import { getProducts, getProductById } from "../controllers/productController.js";
 
-router.get('/:id', asyncHandler( async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if(product) {
-        return res.send(product);
-    } else {
-        res.status(404);
-        throw new Error('Resource Not Found');
-    }
-}));
+router.route('/').get(getProducts);
+
+router.route('/:id').get(getProductById);
 
 export default router;
