c7ed0f47c3be32491dface60f68e88483c67d535
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-01-28 17:24:54 +0530

    shopping cart completed, now authentication pending, learnt about slice, dispatch, handlers, onclick etc

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,12 +9,14 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CartScreen from './screens/CartScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index='true' path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 )
