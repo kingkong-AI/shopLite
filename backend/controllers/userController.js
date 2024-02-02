e8a2f14e68517788d44b13f0b3b74532d877b23d
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-02-02 23:41:05 +0530

    refactored cookie creation to utils and added register user. Also added hashing for the password during registration.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
 
 // @desc Auth user and get token
 // @route POST /api/users/login
@@ -11,23 +11,8 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email: email })
 
     if(user && await user.matchPassword(password)) {
-        const token = jwt.sign({ userId: user._id},
-            process.env.JWT_SECRET,
-            {
-                expiresIn: '30d'
-            });
-
-            // Set Jwt as HTTP Only Cookie
-            res.cookie(
-                'jwt',
-                token,
-                {
-                    httpOnly: true,
-                    secure: process.env.NODE_ENV !== 'development',
-                    sameSite: 'strict',
-                    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-                }
-            );
+        
+        generateToken(res, user._id);
         res.json({
             _id: user._id,
             name: user.name,
@@ -45,13 +30,39 @@ const authUser = asyncHandler(async (req, res) => {
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.send('Register user');
+    const {name, email, password} = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    } 
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if(user) {
+        generateToken(res, user._id);
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
 });
 // @desc Logout User and clear cookie
 // @route POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-    
+
     //setting cookie to empty
     res.cookie('jwt', '', {
         httpOnly: true,
