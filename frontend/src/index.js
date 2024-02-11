5c2f7c07b104a7aa1c305ec516fddc11b22414ea
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-02-11 17:36:14 +0530

    Shipping Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import reportWebVitals from './reportWebVitals';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      <Route path='/shipping' element={<ShippingScreen />} />
     </Route>
   )
 )
