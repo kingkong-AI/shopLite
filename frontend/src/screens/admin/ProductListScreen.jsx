99b34c55250d3b7e1ee72104e32a687cc9578972
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-03-18 18:50:56 +0530

    Added create New product functionality from both frontend and backend

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -3,14 +3,28 @@ import { Table, Button, Row, Col } from "react-bootstrap"
 import { FaTimes, FaEdit, FaTrash } from "react-icons/fa"
 import Message from "../../components/Message"
 import Loader from "../../components/Loader"
-import { useGetProductsQuery } from "../../slices/productsApiSlice"
+import { useGetProductsQuery, useCreateProductMutation } from "../../slices/productsApiSlice"
+import {toast} from 'react-toastify'
 
 const ProductListScreen = () => {
 
-    const {data: products, isLoading, error} = useGetProductsQuery();
+    const {data: products, isLoading, error, refetch} = useGetProductsQuery();
+
+    const [createProduct, {isLoading: loadingCreate}] = useCreateProductMutation();
 
     const deleteHandler = async (id) => {
        
+    }
+
+    const createProductHandler = async () => {
+        if(window.confirm('Are you sure you want to create a new product?')) {
+            try {
+                await createProduct();
+                refetch();
+            } catch (err) {
+                toast.error(err?.data?.message || err.error);
+            }
+        }
     }
 
     return (
@@ -20,11 +34,14 @@ const ProductListScreen = () => {
                     <h1>Products</h1>
                 </Col>
                 <Col className="text-end">
-                    <Button className="btn-sm m-3">
+                    <Button 
+                    className="btn-sm m-3"
+                    onClick={createProductHandler}>
                         <FaEdit />Create Product
                     </Button>
                 </Col>
             </Row>
+            {loadingCreate && <Loader/>}
             {isLoading ? <Loader/> : error ? <Message variant="danger">{error}</Message> : (
                 <>
                     <Table striped hover responsive className="table-sm">
