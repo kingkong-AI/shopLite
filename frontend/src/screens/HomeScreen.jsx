61fbe794b64a47aaf69b5b304c6d754df0e38008
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-03-21 03:52:00 +0530

    Added pagination to both productlistscreen if admin and HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,6 +4,7 @@ import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Paginate from '../components/Paginate';
 
 const HomeScreen = () => {
 
@@ -28,6 +29,7 @@ const HomeScreen = () => {
                 </Col>
             ))}
     </Row>
+    <Paginate pages={data.pages} page={data.page} />
     </>)}
     
     </>
