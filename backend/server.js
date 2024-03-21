503f1c55f87034f32f26621017301e919cd23235
Akshat Jain <akshatjain@Akshats-MacBook-Air-2.local>
2024-03-21 15:53:23 +0530

    Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({extended: true}));
 //Cookie Parser Middleware
 app.use(cookieParser());
 
-app.get('/', (req, res)=> {
-    res.send("API is running ...");
-});
-
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
@@ -36,6 +32,15 @@ app.use('/api/config/paypal', (req, res) => res.send({clientId: process.env.PAYP
 const __dirname = path.resolve(); //Setting __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if(process.env.NODE_ENV === 'production') {
+    //setting static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res)=> {
+        res.send("API is running ...");
+    });
+}
 app.use(notFound);
 app.use(errorHandler);
 
